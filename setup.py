e65d555dda95934c12396914974e9d6689f21181
anandtm0294 <anandtm0294@gmail.com>
2024-03-30 08:43:28 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="ineuron",
+    author_email="anandtm0294@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+
+)
